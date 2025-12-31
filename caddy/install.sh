b2385c833f4dd7b3cc4c593176b61cf376107405
Shan0h <67458534+Shan0h@users.noreply.github.com>
2025-12-31 23:27:03 +0800

    Update install.sh

--- caddy/install.sh
+++ caddy/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/NevermoreSSH/VVV/main/"
+REPO="https://raw.githubusercontent.com/Shan0h/FeverDream/main/"
 
-curl -1sLf 'https://raw.githubusercontent.com/NevermoreSSH/VVV/main/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg --yes  >/dev/null 2>&1
-curl -1sLf 'https://raw.githubusercontent.com/NevermoreSSH/VVV/main/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list  >/dev/null 2>&1
+curl -1sLf 'https://raw.githubusercontent.com/Shan0h/FeverDream/main/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg --yes  >/dev/null 2>&1
+curl -1sLf 'https://raw.githubusercontent.com/Shan0h/FeverDream/main/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list  >/dev/null 2>&1
 sudo apt install caddy
 
 ### Tambah konfigurasi Caddy
